1c2664041e396a16b0d9e3fbe9c2efc3e1ea6c28
José Cerezo <104803561+joseenrique61@users.noreply.github.com>
2026-05-17 22:10:46 +0000

    First cycle

--- src/main/java/GestorPuntos.java
+++ src/main/java/GestorPuntos.java
@@ -3,6 +3,15 @@ import java.util.HashMap;
 
 public class GestorPuntos {
     private Map<String, Integer> jugadores = new HashMap<>();
+    public Map<String, Integer> getJugadores() {
+        return jugadores;
+    }
 
+    public GestorPuntos(Map<String, Integer> jugadores) {
+        this.jugadores = jugadores;
+    }
 
+    public void sumarPuntos(String jugador, int puntos) {
+        jugadores.put(jugador, jugadores.get(jugador) + puntos);
+    }
 }
--- src/test/java/GestorPuntosTest.java
+++ src/test/java/GestorPuntosTest.java
@@ -1,9 +1,16 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
+import java.util.HashMap;
 
 public class GestorPuntosTest {
     @Test
     public void testAniadirPuntos() {
-        
+        Map<String, Integer> jugadores = new HashMap<>();
+        jugadores.put("jugador1", 0);
+        GestorPuntos gestorPuntos = new GestorPuntos(jugadores);
+        gestorPuntos.sumarPuntos("jugador1", 10);
+
+        assertTrue(gestorPuntos.getJugadores().get("jugador1") == 10);
     }
 }
